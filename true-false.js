6eac697067f7cbe0818b779b801f5195dc1a522a
mahamudul98 <mon01774606083@gamil.com>
2024-03-17 16:19:56 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// falsy value :
+// 0
+// ''
+// NaN
+// undefined
+// null
+// false
+
+// let age;
+// console.log(age);
+const age = false;
+if(age){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
