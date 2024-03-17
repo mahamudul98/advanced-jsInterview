203627f68ab67e46348b44a26a1ddb2e6c8fa661
mahamudul98 <mon01774606083@gamil.com>
2024-03-17 16:37:00 +0600

    double-vs-triple-equal

---
+++ double-vs-triple.js
@@ -0,0 +1,21 @@
+// double & triple equal 
+
+// for double equal (==) only check value 
+const num1 = 4;
+const num2 = '4';
+if(num1==num2){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+// for triple equal(===) check value and check data type 
+const num3 = 4;
+const num4 = '4';
+if(num3===num4){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
